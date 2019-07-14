deea6475e105af40dcb59d0ddb810cc2514f9061
Isabella <ikmaki14@gmail.com>
2019-07-14 15:20:44 -0500

    Complete Iteration 0

---
+++ miniprojects/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  miniprojects
+//
+//  Created by Isabella Maki on 7/14/19.
+//  Copyright © 2019 Isabella Maki. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
